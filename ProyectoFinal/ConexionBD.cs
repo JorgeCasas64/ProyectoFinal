94513bfe9391507023b3c4adf69d123d2508958e
Jorge Casas <casas.jorge@uabc.edu.mx>
2015-11-13 08:39:16 -0800

    Creacion del Proyecto Final

---
+++ ProyectoFinal/ConexionBD.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+namespace ProyectoFinal
+{
+    public class ConexionBD : DbContext
+    {
+        public DbSet<ejemplo> Ejemplos { get; set; }
+        
+    }
+}
---
+++ ProyectoFinal/Migrations/201511131625269_PrimeraMigracion.cs
@@ -0,0 +1,26 @@
+namespace ProyectoFinal.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class PrimeraMigracion : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.ejemploes",
+                c => new
+                    {
+                        id = c.Int(nullable: false, identity: true),
+                        Nombre = c.String(),
+                    })
+                .PrimaryKey(t => t.id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.ejemploes");
+        }
+    }
+}
